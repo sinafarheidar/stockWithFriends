c6fd3f1ccfe907025ca148c7cff7a26ac5406ad3
Sina Farheidar-Smith <sinafarheidar12@gmail.com>
2021-01-10 17:04:13 -0800

    Push app to heroku

--- server/models/users.model.js
+++ server/models/users.model.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema
 
 const userSchema = new Schema({
     username: {type: String, required: true, minlength: 3},
-    watchlist: {type: String, required: true, unique: true,minlength: 3}
+    watchlist: {type: String, required: true, unique: true, minlength: 3}
 })
 
 const User = mongoose.model('User', userSchema)
--- server/server.js
+++ server/server.js
@@ -29,7 +29,13 @@ app.get('/', (req, res) => {
 app.use('/stock', stockRouter)
 app.use('/user', userRouter)
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build"))
+  })
+}
 app.listen(port, (req, res) => {
     console.log('Listening on Port: ' + port)
 })
