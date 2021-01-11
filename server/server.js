170e27d8697c0592abb5be0ba52eb00a6a3ef960
Sina Farheidar-Smith <sinafarheidar12@gmail.com>
2021-01-10 22:12:42 -0800

    Inserted path

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const stockRouter = require('./routes/stock')
 const userRouter = require('./routes/user')
 const express = require('express')
+const path = require('path');
 const mongoose = require('mongoose')
 const cors = require('cors');
 
